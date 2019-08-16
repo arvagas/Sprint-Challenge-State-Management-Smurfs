416b6998b0739da75b3d18c42b846c29dc08a5ae
Arvin Agas <arvagas@gmail.com>
2019-08-16 12:47:32 -0400

    renamed instances of FETCH to GET

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-export const FETCH_SMURFS_START = 'FETCH_SMURFS_START'
-export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS'
-export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL'
+export const GET_SMURFS_START = 'GET_SMURFS_START'
+export const GET_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS'
+export const GET_SMURFS_FAIL = 'GET_SMURFS_FAIL'
 
 export const POST_SMURFS_START = 'POST_SMURFS_START'
 export const POST_SMURFS_SUCCESS = 'POST_SMURFS_SUCCESS'
@@ -10,11 +10,11 @@ export const POST_SMURFS_FAIL = 'POST_SMURFS_FAIL'
 
 export const getSmurfVillage = () => {
     return dispatch => {
-        dispatch({ type: FETCH_SMURFS_START })
+        dispatch({ type: GET_SMURFS_START })
         axios
             .get(`http://localhost:3333/smurfs`)
-            .then(res => dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data }))
-            .catch(err => dispatch({ type: FETCH_SMURFS_FAIL, payload: err.response }))
+            .then(res => dispatch({ type: GET_SMURFS_SUCCESS, payload: res.data }))
+            .catch(err => dispatch({ type: GET_SMURFS_FAIL, payload: err.response }))
     }
 }
 
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,7 @@
 import {
-    FETCH_SMURFS_START,
-    FETCH_SMURFS_SUCCESS,
-    FETCH_SMURFS_FAIL,
+    GET_SMURFS_START,
+    GET_SMURFS_SUCCESS,
+    GET_SMURFS_FAIL,
     POST_SMURFS_START,
     POST_SMURFS_SUCCESS,
     POST_SMURFS_FAIL,
@@ -23,20 +23,20 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case FETCH_SMURFS_START :
+        case GET_SMURFS_START :
             return {
                 ...state,
                 callingAPI: true,
                 error: ''
             }
-        case FETCH_SMURFS_SUCCESS :
+        case GET_SMURFS_SUCCESS :
             return {
                 ...state,
                 smurfs: action.payload,
                 callingAPI: false,
                 error: ''
             }
-        case FETCH_SMURFS_FAIL :
+        case GET_SMURFS_FAIL :
             return {
                 ...state,
                 callingAPI: false,
