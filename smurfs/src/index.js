17ff611b265ba88336499c6a385442f0609ee631
Arvin Agas <arvagas@gmail.com>
2019-08-16 11:40:20 -0400

    added formik and redux-thunk dependencies

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,12 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
 import "./index.css"
 import App from "./components/App"
 import { reducer } from './reducers'
 
-const store = createStore(reducer, applyMiddleware())
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
