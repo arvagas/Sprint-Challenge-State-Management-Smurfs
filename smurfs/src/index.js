366cecec108b41ab20fa4449283069a465e4d410
Arvin Agas <arvagas@gmail.com>
2019-08-16 11:36:48 -0400

    set up store, initialstate and reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.css"
+import App from "./components/App"
+import { reducer } from './reducers'
+
+const store = createStore(reducer, applyMiddleware())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+document.getElementById("root"));
